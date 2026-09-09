d57e5add5e0ce1b621d54ed2ce990e06682a2056
Andrés Hernández <tonejito@comunidad.unam.mx>
2026-09-09 00:28:27 -0600

    src: v0.74

--- src/rinetd.h
+++ src/rinetd.h
@@ -1,6 +1,7 @@
 /* Copyright © 1997—1999 Thomas Boutell <boutell@boutell.com>
                          and Boutell.Com, Inc.
              © 2003—2021 Sam Hocevar <sam@hocevar.net>
+             © 2026      Andrés Hernández <tonejito@comunidad.unam.mx>
 
    This software is released for free use under the terms of
    the GNU Public License, version 2 or higher. NO WARRANTY
